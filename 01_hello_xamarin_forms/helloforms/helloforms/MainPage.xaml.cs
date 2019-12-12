1952505d3f45a9126bec777428b14e186648a287
victordomingos <editor.arcosonline@gmail.com>
2019-12-12 18:21:09 +0000

    01_hello_xamarin_forms

---
+++ 01_hello_xamarin_forms/helloforms/helloforms/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace helloforms
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_dont_touch_Clicked(object sender, EventArgs e)
+        {
+            lbl_ola.IsVisible = true;
+            lbl_ola.Text = "Can't you read?";
+            imgbanana.IsVisible = false;
+        }
+
+        private void btn_touch_Clicked(object sender, EventArgs e)
+        {
+            lbl_ola.IsVisible = false;
+            imgbanana.IsVisible = true;
+
+        }
+    }
+}
