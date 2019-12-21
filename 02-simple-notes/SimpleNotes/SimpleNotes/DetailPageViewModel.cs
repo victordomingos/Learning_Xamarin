2816749102eb4a72c7a03eac7a14ac378f3f75b4
victordomingos <editor.arcosonline@gmail.com>
2019-12-21 17:26:50 +0000

    ...

---
+++ 02-simple-notes/SimpleNotes/SimpleNotes/DetailPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SimpleNotes.ViewModels
+{
+    public class DetailPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+
+        public DetailPageViewModel(string note)
+        {
+
+        }
+
+
+
+        string noteText;
+        public string NoteText
+        {
+            get => noteText;
+
+            set
+            {
+                noteText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NoteText)));
+            }
+        }
+
+
+        public Command DismissPageCommand { get; }
+
+    }
+}
